a2d58c6a57ce33f34d72e4fa9ac4f3db93fd726a
ewno92 <77653755+ewno92@users.noreply.github.com>
2023-03-05 01:35:12 -0800

    fix/contactForm

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -10,84 +10,92 @@ const Contact = () => {
   //       }
   //     };
   //   }, [contact]);
+  const [info, setInfo] = useState({});
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+    console.log(info);
+  };
   return (
     <div id="contact">
       {location && <div className="conatct-display">location</div>}
       {contact && (
         <div className="conatct-display">
           <div className="form-body">
-            <div className="form-group">
-              <div className="form-title">
-                <label htmlFor="bc_writer_name">NAME 이름</label>
+            {/* <div className="form-group"> */}
+            {/* <div className="form-title"> */}
+            <label htmlFor="bc_writer_name">NAME 이름</label>
+            {/* </div> */}
+            {/* <div className="form-content"> */}
+            {/* <div className="textarea-wrap"> */}
+            <input
+              className={info?.name && "has-text"}
+              name="name"
+              onChange={handleChange}
+              required="required"
+              placeholder="이름"
+            />
+            {/* </div> */}
+            {/* </div> */}
+            {/* </div> */}
+            {/* <div className="form-group"> */}
+            {/* <div className="form-title"> */}
+            <label htmlFor="bc_subtitle">PHONE NUMBER 전화번호</label>
+            {/* </div> */}
+            {/* <div className="form-content"> */}
+            {/* <div className="textarea-wrap"> */}
+            <input
+              className={info?.number && "has-text"}
+              // className="input-default valid_input"
+              required="required"
+              placeholder="전화번호"
+              name="number"
+              onChange={handleChange}
+              value={info?.number}
+            />
+            {/* </div>
               </div>
-              <div className="form-content">
-                <div className="textarea-wrap">
-                  <input
-                    data-label="이름"
-                    required="required"
-                    placeholder="이름"
-                  />
-                </div>
+            </div> */}
+            {/* <div className="form-group">
+              <div className="form-title"> */}
+            <label htmlFor="bc_writer_email">EMAIL 메일</label>
+            {/* </div> */}
+            {/* <div className="form-content"> */}
+            {/* <div className="textarea-wrap"> */}
+            <input
+              className={info?.email && "has-text"}
+              required="required"
+              placeholder="메일"
+              name="email"
+              onChange={handleChange}
+            />
+            {/* </div>
               </div>
-            </div>
-            <div className="form-group">
-              <div className="form-title">
-                <label htmlFor="bc_subtitle">PHONE NUMBER 전화번호</label>
-              </div>
-              <div className="form-content">
-                <div className="textarea-wrap">
-                  <div className="textarea-placeholder">전화번호</div>
-                  <input
-                    className="input-default valid_input"
-                    required="required"
-                    placeholder="전화번호"
-                    data-label="전화번호"
-                    data-exp="dashnum"
-                    defaultValue=""
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="form-title">
-                <label htmlFor="bc_writer_email">EMAIL 메일</label>
-              </div>
-              <div className="form-content">
-                <div className="textarea-wrap">
-                  <input
-                    required="required"
-                    placeholder="메일"
-                    data-exp="email"
-                    data-label="메일"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="form-group type-content">
-              <div className="form-title">
-                <label htmlFor="bc_content">
-                  MEMO 내용{" "}
-                  <p className="desc">
-                    공사 하실 곳과 평형 수, 공사 시기등을 문의 내용으로
-                    기재해주세요.
-                  </p>
-                </label>
-              </div>
+            </div> */}
+            {/* <div className="form-group type-content"> */}
+            {/* <div className="form-title"> */}
+            <label htmlFor="bc_content">
+              MEMO 내용
+              <p className="desc">
+                공사 하실 곳과 평형 수, 공사 시기등을 문의 내용으로
+                기재해주세요.
+              </p>
+            </label>
+            {/* </div> */}
 
-              <div className="textarea-wrap">
-                <textarea
-                  name="bc_content"
-                  id="bc_content"
-                  className="hide input-textarea valid_input"
-                  required="required"
-                  placeholder="내용"
-                  data-label="내용"
-                  defaultValue={""}
-                />
-                <span className="content-underline" />
-              </div>
-            </div>
+            {/* <div className="textarea-wrap"> */}
+            <textarea
+              className={info?.content && "has-text"}
+              required="required"
+              placeholder="내용"
+              name="content"
+              onChange={handleChange}
+              value={info?.content}
+            />
+            {/* <span className="content-underline" /> */}
+            {/* </div> */}
+            {/* </div> */}
           </div>
         </div>
       )}
